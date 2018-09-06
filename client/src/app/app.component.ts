f6f62c53ecab8c8b9ce035b2d61b384d360ae410
Brian Childress <brian@brianchildress.co>
2018-09-06 07:24:15 -0400

    Adding buttons for testing login/logout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy, OnInit {
+
+  private userSubscription: Subscription;
+  public userName: any = '';
 
   constructor(private authenticationService: AuthenticationService) {}
 
   ngOnInit() {
     this.authenticationService.checkAuthentication();
+    this.userSubscription = this.authenticationService.authenticatedUser.subscribe((userInfo) => {
+      this.userName = userInfo;
+    });
   }
+
+  ngOnDestroy() {
+
+    this.userSubscription.unsubscribe();
+
+  }
+
+  public login() {
+    this.authenticationService.initiateLoginProcess();
+  }
+
+  public logout() {
+    this.authenticationService.initiateLogoutPorcess();
+  }
+
 }
--- client/src/services/authentication.service.ts
+++ client/src/services/authentication.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Observable, Subject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const oauth_config_key = 'oauth_config';
 const oauth_code_key = 'oauth_code';
+const oauth_jwt_key = 'auth';
+const oauth_token_key = 'oauth_token';
 
 @Injectable({
   providedIn: 'root',
@@ -13,17 +15,26 @@ const oauth_code_key = 'oauth_code';
 export class AuthenticationService {
   private apiUrl: String = 'http://localhost:3000';
 
-  private authenticatedUser = new Subject < Object > ();
+  public authenticatedUser = new BehaviorSubject < Object > ('');
   constructor(router: Router, private http: HttpClient) {}
 
+  /**
+   * checkAuthentication - Use to determine if a user is authenticated with the application, if not will initiate the login process
+   */
   public checkAuthentication() {
 
-    // const authToken = this.getAuthToken();
+    const authenticationToken = this.getAuthenticationToken();
 
-    // return false;
+    if (authenticationToken) {
+      return true;
+    }
+
     this.initiateLoginProcess();
   }
 
+  /**
+   * checkUrlParameters - Use to check or examine the browsers url for information that may be passed as part of a redirect uri
+   */
   private checkUrlParameters() {
 
     return Observable.create((observer) => {
@@ -40,14 +51,39 @@ export class AuthenticationService {
 
   }
 
+  /**
+   * cleanupAuthenticationArtifacts - Use to 'clean up' or remove artifacts from local storage
+   * @param { Array } arrayArtifacts - An array of string values representing the name of each localstorage item to be removed
+   */
+  private cleanupAuthenticationArtifacts(arrayArtifacts) {
+
+    arrayArtifacts.forEach(element => {
+      localStorage.removeItem(element);
+    });
+
+  }
+
+  private getAuthenticationToken() {
+    const authenticationToken = localStorage.getItem(oauth_jwt_key);
+    if (authenticationToken) {
+      return authenticationToken;
+    }
+    return false;
+  }
+
   private getOauthConfig() {
 
     return Observable.create((observer) => {
       const oauthConfig = localStorage.getItem(oauth_config_key);
       if (!oauthConfig) {
+        // Cleanup other artifacts here?
+        if (localStorage.getItem(oauth_code_key)) {
+          const arrayToRemove = [oauth_code_key, oauth_jwt_key, oauth_token_key];
+          this.cleanupAuthenticationArtifacts(arrayToRemove);
+        }
         this.http.get(`${this.apiUrl}/oauthparams`).subscribe((response) => {
           localStorage.setItem(oauth_config_key, JSON.stringify(response));
-          return response;
+          observer.next(response);
         }, (error) => {
           console.log(`ERROR => getOauthConfig, ${error}`);
           observer.error(error);
@@ -95,7 +131,8 @@ export class AuthenticationService {
   private getUserInfo(oauthToken) {
     console.log('get user info');
     return Observable.create((observer) => {
-      observer.next();
+      const str = 'ghjitfvbuioi';
+      observer.next(str);
     });
   }
 
@@ -126,7 +163,6 @@ export class AuthenticationService {
 
     return Observable.create((observer) => {
       if (oauthConfig) {
-
         if (oauthCode) {
 
           const httpOptions = {
@@ -140,6 +176,7 @@ export class AuthenticationService {
           const tokenUrl =
             `${oauthConfig.token_url}?code=${oauthCode}&grant_type=authorization_code&redirect_uri=${oauthConfig.token_redirect_uri}`;
           return this.http.post(tokenUrl, {}, httpOptions).subscribe((response) => {
+            localStorage.setItem(oauth_token_key, JSON.stringify(response));
             observer.next(response);
           }, (error) => {
             observer.error(`ERROR: getOauthToken => post returns ${error}`);
@@ -161,16 +198,17 @@ export class AuthenticationService {
   }
 
   private setUserAuthentication(userInfo) {
-    console.log('setUserAuth');
-    return Observable.create((observer) => {
-      observer.next();
-    });
+
+    localStorage.setItem(oauth_jwt_key, JSON.stringify(userInfo));
+    this.authenticatedUser.next(userInfo);
+    const arrayToRemove = [oauth_code_key, oauth_config_key, oauth_token_key];
+    this.cleanupAuthenticationArtifacts(arrayToRemove);
+
   }
 
-  private initiateLogoutPorcess() {
+  public initiateLogoutPorcess() {
     console.log('logging out');
-    return Observable.create((observer) => {
-      observer.next();
-    });
+    const arrayToRemove = [oauth_code_key, oauth_config_key, oauth_jwt_key, oauth_token_key];
+    this.cleanupAuthenticationArtifacts(arrayToRemove);
   }
 }
